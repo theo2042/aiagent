02f2bd8b85ab1562081feaa09646212d182449b3
theo2042 <theo2042@gmail.com>
2026-05-16 03:58:04 +0900

    CH1 L4 complete

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ from google import genai
 
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(response.text)
+
+
 
 
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None:
         raise Exception ("API key not found")
+    client = genai.Client(api_key=api_key)
+    prompt = ("Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=prompt
+    )
+
+
+    
+    if response.usage_metadata == None:
+        raise RuntimeError ("API request failed")
+    
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: \n{response.text}")
+    
 
 
 if __name__ == "__main__":
