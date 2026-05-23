e5fb0c27f846f7fc81912f3c75a029a5efd14984
theo2042 <theo2042@gmail.com>
2026-05-23 14:54:07 +0900

    CH1 L7 complete

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ def main():
 
     parser = argparse.ArgumentParser(description="Ai Agent")
     parser.add_argument("user_prompt", type=str, help="You're out of luck m8")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    
     
     messages: list[types.Content] = [
     types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
@@ -28,9 +30,10 @@ def main():
     if response.usage_metadata == None:
         raise RuntimeError ("API request failed")
     
-    print(f"User prompt: {args.user_prompt}")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: \n{response.text}")
     
 
