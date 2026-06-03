7fc8072ad5bdc39a8add45748865e42ea228f67a
theo2042 <theo2042@gmail.com>
2026-06-03 21:39:32 +0900

    CH2 L2 complete

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,11 +1,9 @@
 from functions.get_files_info import get_files_info
 
-try:
-    get_files_info("calculator", ".")
-    get_files_info("calculator", "/bin")
-    get_files_info("calculator", "../")
-    get_files_info("calculator", "main.py")
+
+print(get_files_info("calculator", "."))
+print(get_files_info("calculator", "/bin"))
+print(get_files_info("calculator", "../"))
+print(get_files_info("calculator", "main.py"))
     
 
-except Exception as e:
-    print(f'Error: {e}')
