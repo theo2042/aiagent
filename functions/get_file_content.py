b7af7b7a4d2d40d08027e768d85c496adfaf35e0
theo2042 <theo2042@gmail.com>
2026-06-21 22:49:38 +0900

    CH2 L4 step 5

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -4,17 +4,22 @@ import config
 def get_file_content(working_directory: str, file_path: str) -> str:
     try:
         working_dir_abs= os.path.abspath(working_directory)
-        target_dir = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        target_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
         # Will be True or False
-        valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-        if not valid_target_dir:
+        valid_target_path = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
+        if not valid_target_path:
             return (f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
-        if not os.path.isfile(target_dir):
+        if not os.path.isfile(target_path):
             return (f'Error: File not found or is not a regular file: "{file_path}"')
         
 
-        with open("example.txt", "r") as f:
-            content = f.read(config.FILE_LIMIT)
+        with open(target_path, "r") as f:
+            MAX_CHARS = config.FILE_LIMIT
+            content = f.read(MAX_CHARS)
+            # After reading the first MAX_CHARS...
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]' 
+            return content
         
         
     
