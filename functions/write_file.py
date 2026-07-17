61320e36a892dd3fd9014b508328a57a8220df3f
theo2042 <theo2042@gmail.com>
2026-07-17 17:16:52 +0900

    CH2 L5 step 3

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,1 +1,18 @@
 def write_file(working_directory: str, file_path: str, content: str) -> str:
+    try:
+        working_dir_abs= os.path.abspath(working_directory)
+        target_path = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        # Will be True or False
+        valid_target_path = os.path.commonpath([working_dir_abs, target_path]) == working_dir_abs
+        if not valid_target_path:
+            return (f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
+        if not os.path.isfile(target_path):
+            return (f'Error: File not found or is not a regular file: "{file_path}"')
+        
+
+        
+
+
+    except Exception as e:
+        return (f"Error: {e}")
+
